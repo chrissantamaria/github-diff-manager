4f0db1d00702e68399a629246afb439dbd5ec9a3
Chris Santamaria <chris@santamaria.me>
2020-08-19 22:59:32 -0400

    Initial tooling setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# GitHub Diff Manager
+
+A tiny Chrome extension providing quality-of-life improvements for code review.
+
+Extension setup heavily inspired by [prettier-chrome-extension](https://github.com/prettier/prettier-chrome-extension) from the Prettier team.
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const TerserPlugin = require('terser-webpack-plugin'); // included as a dependency of webpack
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const MergeJsonPlugin = require('merge-json-webpack-plugin');
+
+const isFirefox = process.env.PLATFORM === 'firefox';
+
+module.exports = ({ outDir, env }) => {
+  const isDevMode = env === 'development';
+
+  return {
+    devtool: false,
+    entry: {
+      content: './src/content/index.js',
+      options: isDevMode
+        ? ['react-devtools', './src/options/index.js']
+        : './src/options/index.js',
+    },
+    mode: env,
+    module: {
+      rules: [
+        {
+          exclude: /node_modules/,
+          test: /\.js$/,
+          use: 'babel-loader',
+        },
+      ],
+    },
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            extractComments: false,
+
+            // https://github.com/webpack-contrib/terser-webpack-plugin/issues/107
+            output: { ascii_only: true },
+          },
+        }),
+      ],
+    },
+    output: {
+      path: outDir,
+    },
+    performance: {
+      hints: false,
+    },
+    plugins: [
+      new HTMLPlugin({
+        chunks: ['options'],
+        filename: 'options.html',
+        template: 'src/options/index.html',
+      }),
+      new MergeJsonPlugin({
+        group: [
+          {
+            files: [
+              'src/manifest.json',
+              isFirefox && 'src/firefox-manifest.json',
+            ].filter(Boolean),
+            to: 'manifest.json',
+          },
+        ],
+      }),
+      !isDevMode && new CleanWebpackPlugin(),
+      new webpack.ProgressPlugin(),
+    ].filter(Boolean),
+    watch: isDevMode,
+  };
+};

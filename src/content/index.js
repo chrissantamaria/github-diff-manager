8903a330c4faab0f4da06cfe8b108221fbc1d7fb
Chris Santamaria <chris@santamaria.me>
2020-08-22 10:28:13 -0400

    Use regex for filename matching

--- src/content/index.js
+++ src/content/index.js
@@ -1,5 +1,7 @@
 // example valid string: /chrissantamaria/github-diff-manager/pull/1/files
 const pathRegex = /^\/[A-Za-z0-9_.-]+\/[A-Za-z0-9_.-]+\/pull\/\d+\/files$/;
+// example valid string: foo.yml
+const fileRegex = /^.+\.yml$/;
 
 if (pathRegex.test(window.location.pathname)) {
   const files = [
@@ -10,7 +12,7 @@ if (pathRegex.test(window.location.pathname)) {
     const filename = file.querySelector('.file-info .link-gray-dark').innerText;
 
     // Example pattern matching, will replace with config
-    if (filename.endsWith('.yml')) {
+    if (fileRegex.test(filename)) {
       const reviewedToggleElement = file.querySelector(
         '.js-reviewed-toggle input'
       );
